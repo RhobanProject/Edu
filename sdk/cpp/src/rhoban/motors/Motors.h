4d5b141a5b9214da4a074117e215061f0a332edb
Victor <victor@victor-OptiPlex-745.(none)>
2012-08-08 12:19:58 +0200

    [sdk][cpp] rhoban/motors config robot - initialisation

---
+++ sdk/cpp/src/rhoban/motors/Motors.h
@@ -0,0 +1,25 @@
+/*************************************************
+ * Publicly released by Rhoban System, August 2012
+ *             www.rhoban-system.fr
+ *
+ * Freely usable for non-commercial purposes
+ *
+ * Licence Creative Commons *CC BY-NC-SA
+ * http://creativecommons.org/licenses/by-nc-sa/3.0
+ *************************************************/
+#ifndef MOTORS_H_
+#define MOTORS_H_
+
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+namespace Rhoban
+{
+
+
+
+}
+
+#endif // MOTORS_H_
---
+++ sdk/cpp/src/rhoban/robot/Robot.h
@@ -0,0 +1,25 @@
+/*************************************************
+ * Publicly released by Rhoban System, August 2012
+ *             www.rhoban-system.fr
+ *
+ * Freely usable for non-commercial purposes
+ *
+ * Licence Creative Commons *CC BY-NC-SA
+ * http://creativecommons.org/licenses/by-nc-sa/3.0
+ *************************************************/
+#ifndef ROBOT_H_
+#define ROBOT_H_
+
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+namespace Rhoban
+{
+
+
+
+}
+
+#endif // ROBOT_H_
